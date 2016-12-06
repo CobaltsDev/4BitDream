2b64c359a7b488996a1d0e896442154811cadb86
Joshua Merrill <jsmerrill2004@gmail.com>
2016-12-05 19:50:55 -0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,5 @@
+public class main() {
+    public static void main(String args[]) {
+        system.out.println("This game is currently in development! We need more developers who will volunteer to work (as in no pay).");
+    }
+}
