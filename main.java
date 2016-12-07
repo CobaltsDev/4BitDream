269d904ea939746a181193c573da5748f80ac7b5
Joshua Merrill <jsmerrill2004@gmail.com>
2016-12-06 20:46:02 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class main() {
     public static void main(String args[]) {
-        system.out.println("This game is currently in development! We need more developers who will volunteer to work (as in no pay).");
+        System.out.println("This game is currently in development! We need more developers who will volunteer to work (as in no pay).");
     }
 }
