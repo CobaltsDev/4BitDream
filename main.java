b9a67cd6f3b6b182c3919848b2a0082c5d187e3a
Jacob Merrill <jrmerrill2004@gmail.com>
2017-02-02 07:51:20 -0800

    added player class

--- main.java
+++ main.java
@@ -1,3 +1,20 @@
+class player {
+    int health;
+    String name;
+    int inventory[10];
+    int[5] hotbar;
+    
+    player(tname) {
+        name = tname;
+        health = 100;
+        for(int i = 0; i<10; i++){
+            inventory[i] = 0;
+        }
+        for(int i = 0; i<5; i++){
+            hotbar[i] = 0;
+        }
+    }
+}
 public class main() {
     public static void main(String args[]) {
         System.out.println("This game is currently in development! We need more developers who will volunteer to work (as in no pay).");
